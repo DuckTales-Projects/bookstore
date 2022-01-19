aff2094221c7ee56d2aefdbed417727945fd942a
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-01-18 21:08:51 -0300

    add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal:true
 
 class Book < ApplicationRecord
+  validates :title,
+            :author,
+            :publisher,
+            :genre,
+            :language,
+            :edition,
+            :place,
+            :year, presence: true
 end
