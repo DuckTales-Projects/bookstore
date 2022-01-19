6ce62f54db5985ad148c9424db63547e76be7dc0
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-01-19 08:54:06 -0300

    Update author_spec.rb

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,8 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  it 'author needs a name' do
+  it 'author registration' do
     author = described_class.new(name: 'John')
     expect(author.name).to eq('John')
+  end
+
+  it 'author needs a name' do
+    expect(described_class.new).not_to be_valid
   end
 end
