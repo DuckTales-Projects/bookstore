836469b07ebd66504b550489df85eb33b6adbd4b
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-01-18 21:31:29 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_221247) do
+ActiveRecord::Schema.define(version: 2022_01_19_000251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
