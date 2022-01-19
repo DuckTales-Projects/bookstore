97949cdace4c12eba480c190f2c3287b22db7324
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-01-19 08:56:20 -0300

    Update publisher_spec.rb

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -3,5 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'publisher registration' do
+    publisher = described_class.new(name: 'Intrínseca')
+    expect(publisher.name).to eq('Intrínseca')
+  end
+
+  it 'publisher needs a name' do
+    expect(described_class.new).not_to be_valid
+  end
 end
