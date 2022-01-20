7643082bb341f2b89d2d6b658377d2b34311478a
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-01-20 19:44:19 -0300

    add publisher test

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -3,4 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, type: :model do
+  it 'publisher registration' do
+    publisher = described_class.new(name: 'Intrínseca')
+    expect(publisher.name).to eq('Intrínseca')
+  end
+
+  it 'publisher needs a name' do
+    expect(described_class.new).not_to be_valid
+  end
 end
