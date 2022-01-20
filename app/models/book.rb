020a807d25fcefd365c060608ad0db71e2cf8676
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-01-20 19:39:49 -0300

    add validations and create language types

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal:true
 
 class Book < ApplicationRecord
+  enum language: { portuguese: 'portuguese', english: 'english', spanish: 'spanish' }
+  validates :title,
+            :author,
+            :publisher,
+            :genre,
+            :language,
+            :edition,
+            :place,
+            :year, presence: true
 end
