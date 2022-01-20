4e203b6082226c0c16ff85969f81c06abc9c57ae
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-01-20 19:42:16 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2022_01_19_003241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
