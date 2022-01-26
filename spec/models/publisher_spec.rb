27596347454788ecf3ecc44d10ebbfaa8a751dd1
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-01-26 16:15:15 -0300

    Update publisher_spec.rb

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -3,12 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, type: :model do
-  it 'publisher registration' do
-    publisher = described_class.new(name: 'Intrínseca')
-    expect(publisher.name).to eq('Intrínseca')
+  subject(:default) { build(:publisher) }
+
+  context 'with shoulda-matchers validations' do
+    it { is_expected.to validate_presence_of(:name) }
   end
 
-  it 'publisher needs a name' do
-    expect(described_class.new).not_to be_valid
+  context 'when the publisher has the default attributes' do
+    it 'must save the author name' do
+      expect(default.save).to be true
+    end
+  end
+
+  context 'when the publisher does not have the default attributes' do
+    it 'wont be valid' do
+      default.name = nil
+      expect(default.valid?).to be false
+    end
+
+    it 'must not save the publisher name' do
+      default.name = nil
+      expect(default.save).to be false
+    end
   end
 end
