901534d7a5d215666ecc68d6b9ce47292765dc55
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-01-26 16:15:13 -0300

    Update author_spec.rb

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,12 +3,27 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  it 'author registration' do
-    author = described_class.new(name: 'John')
-    expect(author.name).to eq('John')
+  subject(:default) { build(:author) }
+
+  context 'with shoulda-matchers validations' do
+    it { is_expected.to validate_presence_of(:name) }
   end
 
-  it 'author needs a name' do
-    expect(described_class.new).not_to be_valid
+  context 'when the author has the default attributes' do
+    it 'must save the author name' do
+      expect(default.save).to be true
+    end
+  end
+
+  context 'when the author does not have the default attributes' do
+    it 'wont be valid' do
+      default.name = nil
+      expect(default.valid?).to be false
+    end
+
+    it 'must not save the author name' do
+      default.name = nil
+      expect(default.save).to be false
+    end
   end
 end
