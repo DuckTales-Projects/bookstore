cd5742ffcc751f841ff7bf69ce6d94af87c51d8b
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-01-26 22:35:42 -0300

    refactoring

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, type: :model do
-  subject(:default) { build(:publisher) }
+  subject(:publisher) { build(:publisher) }
 
   context 'with shoulda-matchers validations' do
     it { is_expected.to validate_presence_of(:name) }
@@ -11,19 +11,15 @@ RSpec.describe Publisher, type: :model do
 
   context 'when the publisher has the default attributes' do
     it 'must save the author name' do
-      expect(default.save).to be true
+      expect(publisher.save).to be true
     end
   end
 
   context 'when the publisher does not have the default attributes' do
     it 'wont be valid' do
-      default.name = nil
-      expect(default.valid?).to be false
-    end
-
-    it 'must not save the publisher name' do
-      default.name = nil
-      expect(default.save).to be false
+      publisher.name = nil
+      expect(publisher.valid?).to be false
+      expect(publisher.save).to be false
     end
   end
 end
