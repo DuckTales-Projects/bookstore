638afe2f6b55bf011430c583f551e5c950107423
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-02-03 14:53:43 -0300

    test 2

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,39 +1,39 @@
 # frozen_string_literal:true
 
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Book, type: :model do
-  subject(:book) { build(:book) }
+# RSpec.describe Book, type: :model do
+#   subject(:book) { build(:book) }
 
-  context 'with validations' do
-    it { is_expected.to define_enum_for(:language).with_values(portuguese: 0, english: 1, spanish: 2) }
-    it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to validate_presence_of(:genre) }
-    it { is_expected.to validate_presence_of(:language) }
-    it { is_expected.to validate_presence_of(:edition) }
-    it { is_expected.to validate_presence_of(:place) }
-    it { is_expected.to validate_presence_of(:year) }
-  end
+#   context 'with validations' do
+#     it { is_expected.to define_enum_for(:language).with_values(portuguese: 0, english: 1, spanish: 2) }
+#     it { is_expected.to validate_presence_of(:title) }
+#     it { is_expected.to validate_presence_of(:genre) }
+#     it { is_expected.to validate_presence_of(:language) }
+#     it { is_expected.to validate_presence_of(:edition) }
+#     it { is_expected.to validate_presence_of(:place) }
+#     it { is_expected.to validate_presence_of(:year) }
+#   end
 
-  context 'with relationships' do
-    it { is_expected.to belong_to(:author) }
-    it { is_expected.to belong_to(:publisher) }
-  end
+#   context 'with relationships' do
+#     it { is_expected.to belong_to(:author) }
+#     it { is_expected.to belong_to(:publisher) }
+#   end
 
-  context 'when the book has the default attributes' do
-    it { expect(book).to be_valid }
+#   context 'when the book has the default attributes' do
+#     it { expect(book).to be_valid }
 
-    it 'must be valid' do
-      book.language = :spanish
-      expect(book).to be_valid
-    end
-  end
+#     it 'must be valid' do
+#       book.language = :spanish
+#       expect(book).to be_valid
+#     end
+#   end
 
-  context 'when the book does not have the default attributes' do
-    it 'wont to be valid' do
-      book.language = nil
-      expect(book.valid?).to be false
-      expect(book.save).to be false
-    end
-  end
-end
+#   context 'when the book does not have the default attributes' do
+#     it 'wont to be valid' do
+#       book.language = nil
+#       expect(book.valid?).to be false
+#       expect(book.save).to be false
+#     end
+#   end
+# end
