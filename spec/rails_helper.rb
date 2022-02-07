42af28e7489abdae47f702fb1f10605fb57460bc
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-02-07 10:02:17 -0300

    SimpleCov config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,11 +3,23 @@
 require 'simplecov'
 require 'codecov'
 
-SimpleCov.start do
-  minimum_coverage 90
+SimpleCov.start 'rails' do
+  add_filter '/spec/'
+  add_filter '/helper/'
+  add_filter '/config/'
+  add_filter '/app/channels/'
+  add_filter '/app/jobs/'
+  add_filter '/app/mailers/'
+  add_filter '/app/controllers/' #temporary filter
+  add_group 'Models', 'app/models'
+  add_group 'Controllers', 'app/controllers'
+
   enable_coverage :branch
+
+  minimum_coverage 90
   minimum_coverage line: 90, branch: 80
   maximum_coverage_drop 2
+
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
