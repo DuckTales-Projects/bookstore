84d45959ad56f0e5f5d8e1c8c478553c0bb18324
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-02-15 14:31:58 -0300

    create show action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
 class BooksController < ApplicationController
+  before_action :filter, only: %i[show]
   def index
     @books = Book.all
 
     render json: @books, status: :ok
+  end
+
+  def show
+    @books = Book.find(params[:id])
+
+    render json: @books, status: :ok
+  end
+
+  private
+
+  def filter
+    return if Book.all.ids.include?(params[:id].to_i)
+
+    render json: { message: "Couldnt find Book with id=#{params[:id]}" }, status: :not_found
   end
 end
