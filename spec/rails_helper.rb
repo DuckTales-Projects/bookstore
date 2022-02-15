2d913968044a2834b0070059efa158f8064dc6b6
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-02-15 14:19:58 -0300

    wip

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,14 +7,12 @@ SimpleCov.start 'rails' do
   add_filter '/app/channels/'
   add_filter '/app/jobs/'
   add_filter '/app/mailers/'
-  add_filter '/app/controllers/' #temporary filter
 
   add_group 'Models', 'app/models'
   add_group 'Controllers', 'app/controllers'
 
   enable_coverage :branch
 
-  minimum_coverage 90
   minimum_coverage line: 90, branch: 80
   maximum_coverage_drop 2
 
