5577466847b3f2695464d7553ee4e06371248191
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-02-15 14:38:39 -0300

    creating the create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,9 @@
 
 class BooksController < ApplicationController
   before_action :filter, only: %i[show]
+  before_action :params_missing?, only: %i[create]
+  before_action :valid_params?, only: %i[create]
+
   def index
     @books = Book.all
 
@@ -14,11 +17,38 @@ class BooksController < ApplicationController
     render json: @books, status: :ok
   end
 
+  def create
+    @book = Book.create(book_params)
+
+    render json: @book, status: :created
+  end
   private
 
   def filter
     return if Book.all.ids.include?(params[:id].to_i)
 
     render json: { message: "Couldnt find Book with id=#{params[:id]}" }, status: :not_found
+  end
+
+  def missing
+    render json: { message: 'invalid or null parameters' }, status: :bad_request
+  end
+
+  def invalid
+    render json: { message: 'invalid or null parameters' }, status: :unprocessable_entity
+  end
+
+  def params_missing?
+    return missing unless params.key?(:book)
+
+    missing unless params.require(:book).keys.count == 8
+  end
+
+  def valid_params?
+    return invalid if params.require(:book).value?(nil)
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :genre, :language, :edition, :place, :year, :publisher_id, :author_id)
   end
 end
