fc515726942f04081d6679e67798c86759696388
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-02-18 16:37:00 -0300

    refactoring

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |error|
+      render json: { message: error.message }, status: :not_found
+    end
+
+    rescue_from ActionController::ParameterMissing do |error|
+      render json: { message: error.message }, status: :bad_request
+    end
+
+    rescue_from ActiveRecord::RecordInvalid do |error|
+      render json: { message: error.message }, status: :unprocessable_entity
+    end
+  end
+end
