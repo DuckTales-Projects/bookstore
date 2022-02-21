60cc67e62232c8fa824abe27c77b3ec29649aad0
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-02-20 21:49:45 -0300

    create index action

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class AuthorsController < ApplicationController
+  def index
+    Author.all.then do |author|
+      render json: author, status: :ok
+    end
+  end
 end
