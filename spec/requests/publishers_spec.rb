41e164d4b2022eb3298dd553ddcbffbf27fa3eb9
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-02-22 13:01:37 -0300

    creating tests for the editors index

--- spec/requests/publishers_spec.rb
+++ spec/requests/publishers_spec.rb
@@ -3,7 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Publishers', type: :request do
-  describe 'GET /index' do
-    pending 'add some examples (or delete)'
+  describe 'GET /publishers' do
+    subject(:get_index) { get publishers_path }
+
+    context 'when publisher exists' do
+      let(:list) { create_list(:publisher, 9) }
+      let(:publisher) { create(:publisher, name: 'Pearson') }
+
+      before do
+        list
+        publisher
+        get_index
+      end
+
+      it 'returns all the publishers' do
+        expect(response).to have_http_status :ok
+        expect(JSON(response.body).size).to eq 10
+        expect(JSON(response.body).last['name']).to eq 'Pearson'
+      end
+    end
+
+    context 'when the publisher does not exist' do
+      before { get_index }
+
+      it 'must return a empty JSON' do
+        expect(response).to have_http_status :ok
+        expect(JSON(response.body).empty?).to eq true
+      end
+    end
   end
 end
