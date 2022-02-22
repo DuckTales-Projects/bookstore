18d6627dc6b08c4810c389a31d4484157f9c0737
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-02-22 13:03:18 -0300

    creating publisher index action

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class PublishersController < ApplicationController
+  def index
+    Publisher.all.then do |publisher|
+      render json: publisher, status: :ok
+    end
+  end
 end
