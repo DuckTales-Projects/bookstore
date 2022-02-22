c2dc1125805622e2659f771784285cc70a792a94
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-02-22 13:11:08 -0300

    creating publisher show tests

--- spec/requests/publishers_spec.rb
+++ spec/requests/publishers_spec.rb
@@ -32,4 +32,32 @@ RSpec.describe 'Publishers', type: :request do
       end
     end
   end
+
+  describe 'GET /publishers/:id' do
+    subject(:show_publisher) { get publisher_path(id) }
+
+    context 'when the publisher exists' do
+      let(:publisher) { create(:publisher, name: 'Bertelsmann') }
+      let(:id) { publisher.id }
+
+      before { show_publisher }
+
+      it 'returns the publisher' do
+        expect(response).to have_http_status :ok
+        expect(JSON(response.body)['name']).to eq 'Bertelsmann'
+      end
+    end
+
+    context 'when the publisher does not exist' do
+      let(:id) { Faker::Number.within(range: 900..1000) }
+      let(:message) { "Couldn't find Publisher with 'id'=#{id}" }
+
+      before { show_publisher }
+
+      it 'is not found' do
+        expect(response).to have_http_status :not_found
+        expect(JSON(response.body)['message']).to eq message
+      end
+    end
+  end
 end
