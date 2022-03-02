635cf19aa6d9e976e5b27231d6b742f2cf989ae3
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-03-02 16:12:21 -0300

    add concern to rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :books, only: %i[index show create update destroy]
-  resources :authors, only: %i[index show create update destroy]
-  resources :publishers, only: %i[index show create update destroy]
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
+
+  resources :books, only: %i[index show create update destroy], concerns: :paginatable
+  resources :authors, only: %i[index show create update destroy], concerns: :paginatable
+  resources :publishers, only: %i[index show create update destroy], concerns: :paginatable
 end
