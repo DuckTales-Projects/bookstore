56808ac213b80f0f70db6600af57dab2a7b34e5a
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-03-03 10:23:15 -0300

    add pagination in controllers

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -2,7 +2,7 @@
 
 class PublishersController < ApplicationController
   def index
-    Publisher.all.then do |publisher|
+    Publisher.order(:id).page(params[:page]).then do |publisher|
       render json: publisher, status: :ok
     end
   end
