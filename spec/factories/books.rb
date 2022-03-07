fbb53dd13844792192cf7d4504889449f5d60736
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-03-07 18:09:52 -0300

    adjusting

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -3,8 +3,8 @@
 FactoryBot.define do
   factory :book do
     title { Faker::Book.title }
-    author { association :author }
-    publisher { association :publisher }
+    author
+    publisher
     genre { Faker::Book.genre }
     language { %w[portuguese english spanish].sample }
     edition { ['first edition', 'second edition', 'third edition'].sample }
