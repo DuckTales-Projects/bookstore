3108ef99d114c875e8a34180b8c4d8ba2070880e
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-03-07 18:09:24 -0300

    add tests for the new author validations

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,26 +5,49 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   subject(:author) { build(:author) }
 
-  context 'with validations' do
-    it { is_expected.to validate_presence_of(:name) }
-  end
-
-  context 'with relationships' do
-    it { is_expected.to have_many(:publishers).through(:books) }
-    it { is_expected.to have_many(:books).dependent(:destroy) }
-  end
-
-  context 'when the author has the default attributes' do
-    it 'must save the author name' do
-      expect(author.save).to be true
+  describe 'Rails validations' do
+    context 'when validations are needed' do
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_length_of(:name).is_at_least(3).is_at_most(20) }
     end
   end
 
-  context 'when the author does not have the default attributes' do
-    it 'wont be valid' do
-      author.name = nil
-      expect(author.valid?).to be false
-      expect(author.save).to be false
+  describe 'relationships' do
+    context 'with relationships' do
+      it { is_expected.to have_many(:publishers).through(:books) }
+      it { is_expected.to have_many(:books).dependent(:destroy) }
+    end
+  end
+
+  describe 'author resources' do
+    context 'when the author has the valid attributes' do
+      it { expect(author).to be_valid }
+    end
+
+    context 'when the author has the invalid attributes' do
+      let(:long_str) { Faker::Name.initials(number: 21) }
+
+      before do
+        author.name = long_str
+        author.save
+      end
+
+      it 'generates the following errors' do
+        expect(author.errors.messages[:name]).to eq ['is too long (maximum is 20 characters)']
+        expect(author.valid?).to be false
+      end
+    end
+
+    context 'when the author has the null attributes' do
+      let(:author) { described_class.new }
+      let(:error) { "can't be blank, is too short (minimum is 3 characters)" }
+
+      before { author.save }
+
+      it 'generates the following errors' do
+        expect(author.errors.messages[:name].join(', ')).to eq error
+        expect(author.valid?).to be false
+      end
     end
   end
 end
