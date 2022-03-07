ada72d7b3246c4165913c0a1957bfcfa5f54dd2a
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-03-07 17:10:42 -0300

    add tests for the new validations

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,35 +5,76 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   subject(:book) { build(:book) }
 
-  context 'with validations' do
-    it { is_expected.to define_enum_for(:language).with_values(portuguese: 0, english: 1, spanish: 2) }
-    it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to validate_presence_of(:genre) }
-    it { is_expected.to validate_presence_of(:language) }
-    it { is_expected.to validate_presence_of(:edition) }
-    it { is_expected.to validate_presence_of(:place) }
-    it { is_expected.to validate_presence_of(:year) }
-  end
-
-  context 'with relationships' do
-    it { is_expected.to belong_to(:author) }
-    it { is_expected.to belong_to(:publisher) }
-  end
-
-  context 'when the book has the default attributes' do
-    it { expect(book).to be_valid }
-
-    it 'must be valid' do
-      book.language = :spanish
-      expect(book).to be_valid
+  describe 'Rails validations' do
+    context 'when validations are needed' do
+      it { is_expected.to define_enum_for(:language).with_values(portuguese: 0, english: 1, spanish: 2) }
+      it { is_expected.to validate_presence_of(:title) }
+      it { is_expected.to validate_presence_of(:genre) }
+      it { is_expected.to validate_presence_of(:language) }
+      it { is_expected.to validate_presence_of(:edition) }
+      it { is_expected.to validate_presence_of(:place) }
+      it { is_expected.to validate_presence_of(:year) }
+      it { is_expected.to validate_numericality_of(:year) }
+      it { is_expected.to validate_length_of(:title).is_at_least(2).is_at_most(50) }
+      it { is_expected.to validate_length_of(:genre).is_at_least(2).is_at_most(50) }
+      it { is_expected.to validate_length_of(:edition).is_at_least(2).is_at_most(50) }
+      it { is_expected.to validate_length_of(:place).is_at_least(2).is_at_most(50) }
     end
   end
 
-  context 'when the book does not have the default attributes' do
-    it 'wont to be valid' do
-      book.language = nil
-      expect(book.valid?).to be false
-      expect(book.save).to be false
+  describe 'relationships' do
+    context 'when relationships are defined' do
+      it { is_expected.to belong_to(:author) }
+      it { is_expected.to belong_to(:publisher) }
+    end
+  end
+
+  describe 'book resources' do
+    context 'when the book has the valid attributes' do
+      it { expect(book).to be_valid }
+    end
+
+    context 'when the book has the invalid attributes' do
+      let(:long_str) { Faker::Name.initials(number: 51) }
+      let(:invalid_year) { [rand(2..1499), rand((Time.zone.now.year + 1)..3000)].sample }
+
+      before do
+        book.title = long_str
+        book.genre = long_str
+        book.edition = long_str
+        book.place = long_str
+        book.year = invalid_year
+        book.save
+      end
+
+      it 'generates the following errors' do
+        expect(book.errors.messages[:title]).to eq ['is too long (maximum is 50 characters)']
+        expect(book.errors.messages[:genre]).to eq ['is too long (maximum is 50 characters)']
+        expect(book.errors.messages[:edition]).to eq ['is too long (maximum is 50 characters)']
+        expect(book.errors.messages[:place]).to eq ['is too long (maximum is 50 characters)']
+        expect(book.errors.messages[:year]).to eq ['must be in 1500..2022']
+        expect(book.valid?).to be false
+      end
+    end
+
+    context 'when the book has the null attributes' do
+      let(:book) { described_class.new }
+      let(:error1) { "can't be blank, is too short (minimum is 2 characters)" }
+      let(:error2) { "is not a number, can't be blank" }
+
+      before { book.save }
+
+      it 'generates the following errors' do
+        expect(book.errors.messages[:title].join(', ')).to eq error1
+        expect(book.errors.messages[:genre].join(', ')).to eq error1
+        expect(book.errors.messages[:edition].join(', ')).to eq error1
+        expect(book.errors.messages[:place].join(', ')).to eq error1
+        expect(book.errors.messages[:language].join).to eq "can't be blank"
+        expect(book.errors.messages[:publisher].join).to eq 'must exist'
+        expect(book.errors.messages[:author].join).to eq 'must exist'
+        expect(book.errors.messages[:year].join(', ')).to eq error2
+        expect(book.valid?).to be false
+      end
     end
   end
 end
