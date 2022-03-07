6a12e1e0f5458950425adcb5f176ccf6d099a0e6
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-03-07 18:10:11 -0300

    add new validation for author

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,5 +4,5 @@ class Author < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :publishers, through: :books
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { in: 3..20 }
 end
