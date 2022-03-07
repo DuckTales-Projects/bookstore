db0eadf3635c3e6a6ee8bf95db6158a61301bfd4
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-03-07 17:11:29 -0300

    add new validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,12 +4,12 @@ class Book < ApplicationRecord
   belongs_to :author
   belongs_to :publisher
 
+  validates :year, numericality: { in: 1500..Time.zone.now.year }
+  validates :year, :language, presence: true
   validates :title,
             :genre,
-            :language,
             :edition,
-            :place,
-            :year, presence: true
+            :place, presence: true, length: { in: 2..50 }
 
   enum language: {
     portuguese: 0,
